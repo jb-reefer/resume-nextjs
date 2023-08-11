e60de27be9db94eb26e5f76b38bd26de629158a7
JB Reefer <jb@jbreefer.nyc>
2023-08-11 10:53:01 -0400

    more meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,9 @@ export const ResumeApp = (props: { Component: any; pageProps: any[] }) => (
       <meta property="twitter:image" content="/public/favicon.ico"></meta>
       <meta property="twitter:title" content="JB Reefer's Resume"></meta>
       <meta property="twitter:description" content=""></meta>
+      <meta property="description" content="JB Reefer's Resume" />
       <meta property="og:image" content="/public/favicon.ico"></meta>
+      <meta property="og:description" content="JB Reefer's Resume" />
     </Head>
     <props.Component {...props.pageProps} />
   </>
