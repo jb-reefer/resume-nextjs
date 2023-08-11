b864a2190717c5930ae4c930045e6b1c73d9b987
JB Reefer <jb@jbreefer.nyc>
2023-08-11 11:11:30 -0400

    Audible update (#9)
    * Added Audible
    
    * url
    
    * set a few meta tags
    
    * more meta tags
    
    * Add Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Analytics } from '@vercel/analytics/react';
 
 import "../styles/globals.css";
 
@@ -6,7 +7,14 @@ export const ResumeApp = (props: { Component: any; pageProps: any[] }) => (
   <>
     <Head>
       <title>JB Reefer's Resume</title>
+      <meta property="twitter:image" content="/public/favicon.ico"></meta>
+      <meta property="twitter:title" content="JB Reefer's Resume"></meta>
+      <meta property="twitter:description" content=""></meta>
+      <meta property="description" content="JB Reefer's Resume" />
+      <meta property="og:image" content="/public/favicon.ico"></meta>
+      <meta property="og:description" content="JB Reefer's Resume" />
     </Head>
+    <Analytics />
     <props.Component {...props.pageProps} />
   </>
 );
