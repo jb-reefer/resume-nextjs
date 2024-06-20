9551824da6ac5612684e314a9cde643f7d33f398
JB Reefer <jb@jbreefer.nyc>
2024-06-20 10:12:48 -0400

    Fix grammar and tense, clarify (#11)
    * grammar
    
    * address change
    
    ---------
    
    Co-authored-by: JB Reefer <jbreefer@amazon.com>

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -21,7 +21,7 @@ export const Hero = (props: any) => (
     <h1 title="Really it's James Brooks Gardner Reefer but this is so much simpler">
       J.B. Reefer
     </h1>
-    <div>New York, NY 10128</div>
+    <div>New York, NY</div>
     <div>(573) 308-2658</div>
     <a href="mailto:jb@jbreefer.nyc?subject=Let's be friends!">
       jb@jbreefer.nyc
