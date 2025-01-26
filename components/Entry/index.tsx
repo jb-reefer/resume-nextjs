ade0d8d0453a7873337282a32a1bd2d54803ed35
JB Reefer <jb@jbreefer.nyc>
2025-01-26 15:01:57 -0500

    run format

--- components/Entry/index.tsx
+++ components/Entry/index.tsx
@@ -4,6 +4,4 @@ const StyledList = styled.li`
   margin: 0.5rem;
 `;
 
-export const Entry = (props: { children: any }) => (
-  <StyledList>{props.children}</StyledList>
-);
+export const Entry = (props: { children: any }) => <StyledList>{props.children}</StyledList>;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/globals.css";
 
